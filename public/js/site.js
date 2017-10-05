47439c4239309cfe5bcb43ad21b4dd9b3c13d349
Daniel Adigun <daniel.adigun@digitalforte.ng>
2017-10-05 11:23:19 +0100

    updates

--- public/js/site.js
+++ public/js/site.js
@@ -1,1 +1,20 @@
 ﻿// Write your Javascript code.
+(function($){
+    $(document).ready(function(){
+
+        $(function(){
+            $(window).scroll(function(){
+                if($(this).scrollTop() > 100){
+                    //remove logo
+                    $('.navbar-brand').fadeOut();
+                    
+                }else{
+                    //show logo
+                    $('.navbar-brand').fadeIn();
+                    
+                }
+            })
+        })
+
+    });
+}(jQuery));
